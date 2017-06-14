434dd4f43127b819c4d13aa91f498bf453590662
KarinaKorsgaard <kkorsgaardjensen@gmail.com>
2017-06-14 12:53:37 +0200

    vol adjust

--- src/mumble.h
+++ src/mumble.h
@@ -45,26 +45,27 @@ public:
         
         //
         for(int i = 0; i<sp.size();i++){
-            if(vol > 0){
-                Player * p = &sp[i];
-                p->age++;
-                if(p->age>60*60)p->volume-=0.1;
-                else if (p->age<30)p->volume+=0.01;
-                else if (ofRandom(1)>.5)p->volume+=0.01;
-                else p->volume-=0.01;
-                
-                if(p->volume<0. || (!p->soundPlayer.isPlaying() && p->age>10)){
-                    if(files.size()>0){
-                        int sIndx = int(ofRandom(files.size()-1));
-                        p->soundPlayer.unload();
-                        p->soundPlayer.load(files[sIndx]);
-                        p->age = 0;
-                        p->volume = 0;
-                        p->soundIndx = sIndx;
-                        p->soundPlayer.play();
-                    }
+            Player * p = &sp[i];
+            p->age++;
+            
+            //if(vol > 0){
+            if(p->age>60*60)p->volume-=0.1;
+            else if (p->age<30)p->volume+=0.01;
+            else if (ofRandom(1)>.5)p->volume+=0.01;
+            else p->volume-=0.01;
+            
+            if(p->volume<0. || (!p->soundPlayer.isPlaying() && p->age>10)){
+                if(files.size()>0){
+                    int sIndx = int(ofRandom(files.size()-1));
+                    p->soundPlayer.unload();
+                    p->soundPlayer.load(files[sIndx]);
+                    p->age = 0;
+                    p->volume = 0;
+                    p->soundIndx = sIndx;
+                    p->soundPlayer.play();
                 }
             }
+
             p->volume=CLAMP(p->volume,0,1);
             p->soundPlayer.setVolume(p->volume * MAX(vol,0) );
         }
